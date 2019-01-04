236af0d1abeaba279c3ab82baaf76dc0020decf5
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-04 19:42:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -20,6 +20,6 @@ speak_to_grandma(phrase)
   else
     puts "NO, NOT SINCE 1938!"
   end
-end    
-     
+end
+
     
