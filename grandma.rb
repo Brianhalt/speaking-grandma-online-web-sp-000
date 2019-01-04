bde161617f756d40c50b60520eb342e2f8a73e0b
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-04 19:45:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = uppercase
+  elsif phrase.uppercase
     puts "HUH?! SPEAK UP, SONNY!"
   else
     puts "NO, NOT SINCE 1938!"
